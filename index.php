d11e615b52679f7a14e2d681c4a2adfc81436ee7
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-09-29 09:33:23 -0300

    commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    // Inicializa o contador como 4
+    $contador = 4;
+
+    // Loop que vai até o número 30 com incrementos de 2 em 2
+    while ($contador <= 30) {
+        // Verifica se o contador chegou em 24 e, se sim, para o loop
+        if ($contador == 24) {
+            break;
+        }
+
+        // Imprime o valor atual do contador
+        echo $contador . "<br>";
+
+        // Incrementa o contador de 2 em 2
+        $contador += 2;
+    }
+?>
